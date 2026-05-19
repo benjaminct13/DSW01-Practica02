6843bf52d05e87690dff4f10e12ee3c015539661
benjaminct <benjamincoello13@gmail.com>
2026-05-19 04:21:04 -0600

    config: migrate Cypress config from TypeScript to JavaScript

--- apps/frontend/cypress.config.js
+++ apps/frontend/cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
